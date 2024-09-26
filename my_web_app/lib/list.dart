72c625c44e2557351dccb182b1a040b3f1bfa1ed
IshimuraIyori <s2211636@s.tsukuba.ac.jp>
2024-09-26 16:39:18 +0900

    リストで、名前場所時間が表示できました。

--- my_web_app/lib/list.dart
+++ my_web_app/lib/list.dart
@@ -11,41 +11,102 @@ class NextPage extends StatelessWidget {
           backgroundColor: const Color(0xFF00FF00),
         ),
         body: Center(
-          child: ListView(
-            children: <Widget>[
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('ゆきと'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
+            child: ListView(children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Text('Name'),
+                const Text('Deadline'),
+                const Text('Place'),
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Container(
+              width: 20,
+              height: 20,
+              child: Image.asset(
+                'images/mob.png',
+                fit: BoxFit.contain,
               ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('ひろと'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text('いより'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          )
-        )
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('ゆうき'),
+                Text('~12:00'),
+                Text('3学'),
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('ゆきと'),
+                Text('~12:00'),
+                Text('3学'),
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.person),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Text('いより'),
+                Text('~12:00'),
+                Text('3学'),
+              ],
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ]
 
-        // body: Center(
-        //   child: ElevatedButton(
-        //     onPressed: () {
-        //       Navigator.pop(context);
-        //     },
-        //     child: const Text('Go back!'),
-        //   ),
-        // ),
-        );
+                //     ListTile(
+                //       leading: const Icon(Icons.person),
+                //       title: const Text('ひろと'),
+                //       subtitle: Row(children: <Widget>[
+                //         Text('12:00'),
+                //         SizedBox(width: 10),
+                //         Text('3学'),
+                //       ]),
+                //       onTap: () {
+                //         Navigator.pop(context);
+                //       },
+                //     ),
+                //     ListTile(
+                //       leading: const Icon(Icons.person),
+                //       title: const Text('いより'),
+                //       onTap: () {
+                //         Navigator.pop(context);
+                //       },
+                //     ),
+                //   ],
+                // ))
+
+                // body: Center(
+                //   child: ElevatedButton(
+                //     onPressed: () {
+                //       Navigator.pop(context);
+                //     },
+                //     child: const Text('Go back!'),
+                //   ),
+                // ),
+                )));
   }
 }
