8738bbb9b078d891158855d47a7addc755e855ee
MonyKazu <0av6w84t38t580w@au.com>
2024-09-27 13:15:58 +0900

    プロダクト概要ページの復活

--- my_web_app/lib/main.dart
+++ my_web_app/lib/main.dart
@@ -1,59 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:my_web_app/list.dart';
 
 void main() {
-  runApp(const LoginSample());
+  runApp(const MyApp());
 }
 
-class LoginSample extends StatelessWidget {
-  const LoginSample({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Login Sample',
-      // home: const MyHomePage(title: 'Login Sample'),
-      home: LoginPage(),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // メールアドレス入力
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'メールアドレス'),
-                onChanged: (String value) {},
-              ),
-              // パスワード入力
-              TextFormField(
-                decoration: const InputDecoration(labelText: 'パスワード'),
-                obscureText: true,
-                onChanged: (String value) {},
-              ),
-
-              SizedBox(
-                width: double.infinity,
-                // ユーザー登録ボタン
-                child: ElevatedButton(
-                  child: const Text('ユーザー登録'),
-                  onPressed: () {
-                    debugPrint('ユーザー登録');
-                  },
-                ),
-              )
-            ],
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: [
+          // Container to set the background color
+          Container(
+            color: Colors.white, // Set background color here
           ),
-        ),
+          Center(
+            child: Image.asset('images/ひマッチ@4x-100.jpg'),
+          ),
+          Align(
+            alignment: Alignment.centerRight, // 中央の右端に配置
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0), // 右端に少し余白を追加
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => NextPage()));
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[300],
+                  foregroundColor: Colors.purple,
+                ),
+                child: const Text('Button'),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
