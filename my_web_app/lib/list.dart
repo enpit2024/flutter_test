7b0239e4ef8c297963da3848058a52bf1c4de637
MonyKazu <0av6w84t38t580w@au.com>
2024-09-27 13:26:41 +0900

    ボタンナビゲーションバーの追加

--- my_web_app/lib/list.dart
+++ my_web_app/lib/list.dart
@@ -6,115 +6,125 @@ class NextPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('暇な人リスト'),
-          backgroundColor: const Color(0xFF00FF00),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('暇な人リスト'),
+        backgroundColor: const Color(0xFF00FF00),
+      ),
+      body: Center(
+          child: ListView(children: <Widget>[
+        ListTile(
+          leading: const Icon(Icons.person),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              const Text('Name'),
+              const Text('Deadline'),
+              const Text('Place'),
+            ],
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
         ),
-        body: Center(
-            child: ListView(children: <Widget>[
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                const Text('Name'),
-                const Text('Deadline'),
-                const Text('Place'),
-              ],
+        ListTile(
+          leading: Container(
+            width: 20,
+            height: 20,
+            child: Image.asset(
+              'images/mob.png',
+              fit: BoxFit.contain,
             ),
-            onTap: () {
-              Navigator.pop(context);
-            },
           ),
-          ListTile(
-            leading: Container(
-              width: 20,
-              height: 20,
-              child: Image.asset(
-                'images/mob.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('ゆうき'),
-                Text('~12:00'),
-                Text('3学'),
-              ],
-            ),
-            
-            onTap: () {
-              Navigator.pop(context);
-            },
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('ゆうき'),
+              Text('~12:00'),
+              Text('3学'),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('ゆきと'),
-                Text('~12:00'),
-                Text('3学'),
-              ],
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.person),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('ゆきと'),
+              Text('~12:00'),
+              Text('3学'),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.person),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text('いより'),
-                Text('~12:00'),
-                Text('3学'),
-              ],
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.person),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('いより'),
+              Text('~12:00'),
+              Text('3学'),
+            ],
           ),
-          FloatingActionButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Icon(Icons.add),
-          ),
-        ]))
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ]
 
-                //     ListTile(
-                //       leading: const Icon(Icons.person),
-                //       title: const Text('ひろと'),
-                //       subtitle: Row(children: <Widget>[
-                //         Text('12:00'),
-                //         SizedBox(width: 10),
-                //         Text('3学'),
-                //       ]),
-                //       onTap: () {
-                //         Navigator.pop(context);
-                //       },
-                //     ),
-                //     ListTile(
-                //       leading: const Icon(Icons.person),
-                //       title: const Text('いより'),
-                //       onTap: () {
-                //         Navigator.pop(context);
-                //       },
-                //     ),
-                //   ],
-                // ))
+              //     ListTile(
+              //       leading: const Icon(Icons.person),
+              //       title: const Text('ひろと'),
+              //       subtitle: Row(children: <Widget>[
+              //         Text('12:00'),
+              //         SizedBox(width: 10),
+              //         Text('3学'),
+              //       ]),
+              //       onTap: () {
+              //         Navigator.pop(context);
+              //       },
+              //     ),
+              //     ListTile(
+              //       leading: const Icon(Icons.person),
+              //       title: const Text('いより'),
+              //       onTap: () {
+              //         Navigator.pop(context);
+              //       },
+              //     ),
+              //   ],
+              // ))
 
-                // body: Center(
-                //   child: ElevatedButton(
-                //     onPressed: () {
-                //       Navigator.pop(context);
-                //     },
-                //     child: const Text('Go back!'),
-                //   ),
-                // ),
-                );
+              // body: Center(
+              //   child: ElevatedButton(
+              //     onPressed: () {
+              //       Navigator.pop(context);
+              //     },
+              //     child: const Text('Go back!'),
+              //   ),
+              // ),
+              )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
+    );
   }
 }
