9f66059314ade6eb8b917570850258024935ff84
MonyKazu <0av6w84t38t580w@au.com>
2024-10-23 12:58:38 +0900

    first commit

--- my_web_app/lib/model/himapeople.dart
+++ my_web_app/lib/model/himapeople.dart
@@ -4,20 +4,22 @@ class HimaPeople {
   String id;
   String name;
   bool isHima;
+  String mail;
 
   HimaPeople({
     required this.id,
     required this.name,
     required this.isHima,
+    required this.mail,
   });
 
   factory HimaPeople.fromFirestore(DocumentSnapshot<Map<String, dynamic>> doc) {
     final data = doc.data()!;
     return HimaPeople(
-      id: data['id'],
-      name: data['name'],
-      isHima: data['isHima'],
-    );
+        id: data['id'],
+        name: data['name'],
+        isHima: data['isHima'],
+        mail: data['mail']);
   }
 
   Map<String, dynamic> toFirestore() {
@@ -25,6 +27,7 @@ class HimaPeople {
       "id": id,
       "name": name,
       "isHima": isHima,
+      "mail": mail,
     };
   }
 }
